b3b869e8933dbede086cbb6b7f6e6dbe02bdf51e
Karthikeyan6697 <152838635+Karthikeyan6697@users.noreply.github.com>
2024-02-06 12:34:21 +0530

    Add files via upload

---
+++ Myproject/Myapp/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from .models import *
+
+
+class CategoryAdmin(admin.ModelAdmin):
+	list_display = ('name', 'description')
+class ProductAdmin(admin.ModelAdmin):
+	list_display = ('name', 'description')
+
+admin.site.register(Category,CategoryAdmin)
+admin.site.register(Product,ProductAdmin)
